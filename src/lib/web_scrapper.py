ccc8e0b680f926741a6d89cb1dd33cc366941d67
Alpha Dragon <admin@backendforge.com>
2024-04-30 09:33:12 +0200

    fix broken code

--- src/lib/web_scrapper.py
+++ src/lib/web_scrapper.py
@@ -136,9 +136,10 @@ class ScraperThread:
     @property
     def signal_start(self):
         return self._signal_start
-    
+
     @signal_start.setter
     def signal_start(self, value: int):
+        self._signal_start = value
 
     @property
     def scraper(self):
@@ -169,7 +170,11 @@ class ScraperThread:
         self.series = np.array(
             [
                 [
-                    int(datetime.datetime.strptime(data['timestamp'], "%Y-%m-%dT%H:%M:%S").timestamp()),
+                    int(
+                        datetime.datetime.strptime(
+                            data["timestamp"], "%Y-%m-%dT%H:%M:%S"
+                        ).timestamp()
+                    ),
                     float(data["open"]),
                     float(data["high"]),
                     float(data["low"]),
