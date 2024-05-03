79256aaefb45119d0793476fb6f5a7ee909e7cc7
Alpha Dragon <admin@backendforge.com>
2024-05-03 20:04:15 +0200

    rug logic

--- src/lib/web_scrapper.py
+++ src/lib/web_scrapper.py
@@ -281,9 +281,9 @@ class ScraperThread:
 
         for key, val in price_change.items():
             price_change[key] = float(val)
-        if price_change["m5"] < -75:
+        if price_change["m5"] < -50:
             return True
-        elif price_change["h1"] < -85:
+        elif price_change["h1"] < -80:
             return True
         elif price_change["h6"] < -90 or price_change["h24"] < -95:
             return True
